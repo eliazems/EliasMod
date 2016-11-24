2fe90f11c6b5a6b1a7d1c00913b411c15763880a
eliazems <eliasmonteiro04@hotmail.com>
2016-11-24 13:20:35 -0200

    Nova Worktable Tile, Phazon Core
    acrescentei a mesa "Nova Worktable" ao mod, ela serve para o craft de "pure Phazon" e das armaduras mais avançadas .

---
+++ EliasMod/Tiles/NovaWorkTableTile.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EliasMod.Tiles
+{
+    public class NovaWorkTableTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            AddMapEntry(new Color(200, 200, 200), "Nova worktable");
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+            disableSmartCursor = true;
+            TileObjectData.addTile(Type);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 16, mod.ItemType("NovaWorkTable"));
+        }
+    }
+}
--- EliasMod/Tiles/PhazonCoreTile.cs
+++ EliasMod/Tiles/PhazonCoreTile.cs
@@ -4,7 +4,7 @@ using Terraria.ModLoader;
 
 namespace EliasMod.Tiles
 {
-    public class PhazonTile : ModTile
+    public class PhazonCoreTile : ModTile
     {
         public override void SetDefaults()
         {
@@ -12,16 +12,16 @@ namespace EliasMod.Tiles
             Main.tileMergeDirt[Type] = true;
             Main.tileBlockLight[Type] = true;  //true for block to emit light
             Main.tileLighted[Type] = true;
-            drop = mod.ItemType("Phazon");   //put your CustomBlock name
-            AddMapEntry(new Color(85, 223, 255), "Phazon");
+            drop = mod.ItemType("PhazonCore");   //put your CustomBlock name
+            AddMapEntry(new Color(255, 65, 0), "Phazon Core");
             minPick = 250;
             soundType = 21;  //som certo pra isso, que no caso é o da picareta na pedra
-            dustType = 13;  //a 13 tem a cor exata do minério
+            dustType = 28;  //a 13 tem a cor exata do minério
         }
         /*public override void Effects(Player player, int index)
         {
             //player.lifeRegen -= 60;
-            player.AddBuff(ModifyLight.BuffType("PhazonDebuff"), 400);
+            player.AddBuff(ModifyLight.BuffType("Phazon Radiation"), 400);
 
         }*/
         public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)   //light colors
@@ -29,9 +29,9 @@ namespace EliasMod.Tiles
             //r = 0.5f;
             //g = 0.5f;
             //b = 0.5f;
-            r = (85f / 255f);
-            g = (223f / 255f);
-            b = (255f / 255f);
+            r = (255f / 255f);
+            g = (105f / 255f);
+            b = (0f / 255f);
         }
     }
 }
